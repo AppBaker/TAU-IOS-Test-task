f3a7dc071ad37431c6fee309eb25a5b2df8ee1e9
IvanNikitin <nqqaqq@gmail.com>
2019-11-28 00:24:19 +0700

    Initial Commit

---
+++ TAU IOS Test task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TAU IOS Test task
+//
+//  Created by Ivan Nikitin on 28.11.2019.
+//  Copyright © 2019 Ivan Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
