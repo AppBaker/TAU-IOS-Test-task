869eefde896ac8348df360b6dcf5f7894f9cacf9
IvanNikitin <nqqaqq@gmail.com>
2019-11-28 13:41:11 +0700

    Pod Install GMS

---
+++ TAU IOS Test task/Supporting files/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TAU IOS Test task
+//
+//  Created by Ivan Nikitin on 28.11.2019.
+//  Copyright © 2019 Ivan Nikitin. All rights reserved.
+//
+
+struct K {
+    struct GoogleMap {
+        static let apiKey = "AIzaSyDCkCA1rA9YykTZ_qbMRk1uTBW9LzMiLT4"
+    }
+}
